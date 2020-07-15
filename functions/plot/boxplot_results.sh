fc4d7d77cdbd312b775b215b7b32c3d030a39ecb
syousefi <sahar_yousefi@ymail.com>
2020-07-15 16:00:32 +0200

    shell script for plotting the boxplots

--- functions/plot/boxplot_results.sh
+++ functions/plot/boxplot_results.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=measures
-#SBATCH --output=/exports/lkeb-hpc/syousefi/2-lkeb-17-dl01/syousefi/TestCode/EsophagusProject/Code/Log_2019_09_23/Dataset3/33533_0.75_4-train1-04252020_220/result/plot.txt
+#SBATCH --output=/exports/lkeb-hpc/syousefi/2-lkeb-17-dl01/syousefi/TestCode/EsophagusProject/Code/Log_2019_09_23/Dataset3/33533_0.75_4-train1-07032020_170/plot.txt
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
-#SBATCH --mem-per-cpu=5200
-#SBATCH --partition=gpu
+#SBATCH --mem-per-cpu=6200
+#SBATCH --partition=LKEBgpu
 #SBATCH --gres=gpu:1
 #SBATCH --time=0
-#SBATCH --nodelist=res-hpc-gpu01
+#SBATCH --nodelist=res-hpc-lkeb05
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/exports/lkeb-hpc/syousefi/Programs/cuda9.0/cuda/lib64/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/share/software/NVIDIA/cuda-9.0/extras/CUPTI/lib64/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/share/software/NVIDIA/cuda-9.0/lib64/
