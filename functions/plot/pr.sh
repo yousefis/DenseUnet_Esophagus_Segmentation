616a6158913e0efccef161e034f3db6857be9373
syousefi <sahar_yousefi@ymail.com>
2020-10-11 00:38:12 +0200

    PR curve

--- functions/plot/pr.sh
+++ functions/plot/pr.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=tst_att
-#SBATCH --output=/exports/lkeb-hpc/syousefi/2-lkeb-17-dl01/syousefi/TestCode/EsophagusProject/Code/Log_2019_09_23/Dataset3/33533_0.75_4-train1-08242020_1950240/result/output1.txt
+#SBATCH --output=/exports/lkeb-hpc/syousefi/2-lkeb-17-dl01/syousefi/TestCode/EsophagusProject/Code/Log_2019_09_23/Dataset3/33533_0.75_4-train1-04172020_140/result_vali/output1.txt
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=15
+#SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=4200
-#SBATCH --partition=gpu
+#SBATCH --partition=LKEBgpu
 #SBATCH --gres=gpu:1
 #SBATCH --time=0
-#SBATCH --nodelist=res-hpc-gpu01
+#SBATCH --nodelist=res-hpc-lkeb05
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/exports/lkeb-hpc/syousefi/Programs/cuda9.0/cuda/lib64/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/share/software/NVIDIA/cuda-9.0/extras/CUPTI/lib64/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/share/software/NVIDIA/cuda-9.0/lib64/
