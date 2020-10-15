3e6063cce7949ff55a8ff8c599e544177163bc92
syousefi <sahar_yousefi@ymail.com>
2020-10-15 11:29:55 +0200

    network submitter on server

---
+++ submit_on_server/submit_test_job_distancemap_attention.py
@@ -0,0 +1,5 @@
+import os
+
+
+cmd='sbatch /exports/lkeb-hpc/syousefi/2-lkeb-17-dl01/syousefi/TestCode/EsophagusProject/Code/dense_net_3d_segmentation-1-dice-tumor--106/test_job_distancemap_attention_surface.sh'
+os.system(cmd)
--- submit_test_job_distancemap.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-cmd='sbatch /exports/lkeb-hpc/syousefi/2-lkeb-17-dl01/syousefi/TestCode/EsophagusProject/Code/dense_net_3d_segmentation-1-dice-tumor--106/test_job_distancemap_attention.sh'
-os.system(cmd)
